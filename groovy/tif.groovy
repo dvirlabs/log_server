1e75328d68fc31a290365a4b0d9bdc9eeea5303c
TiferetJackson <117598371+TiferetJackson@users.noreply.github.com>
2024-02-18 00:56:58 +0200

    Update tif.groovy

--- groovy/tif.groovy
+++ groovy/tif.groovy
@@ -5,27 +5,10 @@ pipeline {
 
         stage("build") {
             steps {
-                script {
-                    def remoteHost = '192.168.1.30'
-                    def remoteUser = 'root'
-                    def remotePassword = 'Aa123456'
-
-                    // SSH command to be executed on the remote server
-                    def command = 'echo "Hello from Jenkins" > /tmp/tif-groovy.txt'
-
-                    // Execute SSH command
-                    // sshCommand remote: [
-                    //     host: remoteHost,
-                    //     user: remoteUser,
-                    //     password: remotePassword,
-                    //     allowAnyHosts: true // Allow connecting to any host (not recommended for production)
-                    // ], command: command
-
-                    // Execute SSH command using sh step
-                    sh(script: """
-                        sshpass -p ${remotePassword} ssh -o StrictHostKeyChecking=no -o PreferredAuthentications=password ${remoteUser}@${remoteHost} '${command}'
-                    """)
-                }
+                 sshagent (credentials: ['123456']) {
+                        sh 'ssh -o StrictHostKeyChecking=no -l root 192.168.1.30'
+                        sh 'echo tif > /tmp/tif.txt'
+                    }
             }
         }
 
