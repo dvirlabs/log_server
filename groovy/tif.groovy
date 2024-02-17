20cfd28042246f256d4e549ae43bddd373fc41df
TiferetJackson <117598371+TiferetJackson@users.noreply.github.com>
2024-02-18 01:17:11 +0200

    Update tif.groovy

--- groovy/tif.groovy
+++ groovy/tif.groovy
@@ -6,7 +6,7 @@ pipeline {
         stage("build") {
             steps {
                  sshagent (credentials: ['123456']) {
-                        sh 'sudo ssh -i /tmp/tif root@192.168.1.30'
+                        sh 'sudo ssh -o StrictHostKeyChecking=no -i /tmp/tif root@192.168.1.30'
                         sh 'echo tif > /tmp/tif.txt'
                     }
             }
