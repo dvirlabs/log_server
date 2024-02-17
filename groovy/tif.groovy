73b4fb6aca3575243e70bfe6e107922da4ddaf94
Tiferet Jackson <tif@syslog-server.pi.hole>
2024-02-17 21:52:58 +0000

    Fix groovy file

--- groovy/tif.groovy
+++ groovy/tif.groovy
@@ -7,6 +7,23 @@ pipeline {
             steps {
                 echo 'building the application...'
                 sh "echo tif build"
+
+                script {
+                    def remoteHost = '192.168.1.30'
+                    def remoteUser = 'root'
+                    def remotePassword = 'Aa123456'
+
+                    // SSH command to be executed on the remote server
+                    def command = 'echo "Hello from Jenkins" > /tmp/tif-groovy.txt'
+
+                    // Execute SSH command
+                    sshCommand remote: [
+                        host: remoteHost,
+                        user: remoteUser,
+                        password: remotePassword,
+                        allowAnyHosts: true // Allow connecting to any host (not recommended for production)
+                    ], command: command
+                }
             }
         }
 
