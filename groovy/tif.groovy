7f7057166d17fd93eef621e500eb951160d1a1eb
Tiferet Jackson <tif@syslog-server.pi.hole>
2024-02-17 21:39:30 +0000

    Fix groovy file

--- groovy/tif.groovy
+++ groovy/tif.groovy
@@ -1,32 +1,13 @@
 pipeline {
     agent any
-    environment {
-        DOCKER_IMAGE = "dvirlabs/jenkins-test:${env.BUILD_TAG}"
-    }
 
     stages {
 
         stage("build") {
             steps {
                 echo 'building the application...'
-                sh "docker build -t ${DOCKER_IMAGE} ."
+                sh "echo tif build"
             }
-        }
-
-
-        stage("login") {
-            steps {
-                echo 'login to Dockerhub...' 
-                sh 'docker login -u dvirlabs -p dckr_pat_pPy6gAAksSYnRq0SUFo_-XML9hY'
-            }
-        }
-
-        stage("Push image") {
-            steps {
-                echo 'push to dockerhub...'
-                sh "docker push ${DOCKER_IMAGE}"
-            }
-
         }
 
     }
